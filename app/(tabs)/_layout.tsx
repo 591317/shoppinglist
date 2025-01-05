fd0af7bde4c36a63e3a1c4485d2b4070387e3edf
EricDaCosta <591317@stud.hvl.no>
2025-01-05 18:38:06 +0100

    change name to Shopping List for header in home

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router'
 const Layout = () => {
   return (
    <Stack>
-    <Stack.Screen name="home" options={{headerTitle: "Your List",  headerTitleAlign: 'center'}}/>
+    <Stack.Screen name="home" options={{headerTitle: "Shopping List",  headerTitleAlign: 'center'}}/>
    </Stack>
   )
 }
